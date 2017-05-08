8d0490d87af7da4903ad924d9c923472cf664792
Karsten Westra <karstenwestra@gmail.com>
2017-05-08 23:19:30 +0200

    Mapped out Event Image.

---
+++ app/src/main/java/com/gifgroen/ticketmaster/model/Image.java
@@ -0,0 +1,13 @@
+package com.gifgroen.ticketmaster.model;
+
+/**
+ * API model class describing a ticketmaster image.
+ */
+class Image {
+    String url;
+    String ratio;
+    int width;
+    int height;
+    boolean fallback;
+    String attribution;
+}
